341dc18a89f01820198cba38c42fffbfdd692c62
Dmitriy Saveliev <savelievdima2016@yandex.ru>
2021-07-22 11:42:32 +0300

    Initial Commit

---
+++ Lesson 4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson 4
+//
+//  Created by Дмитрий on 22.07.2021.
+//  Copyright © 2021 Dimitriy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
