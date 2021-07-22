ced9fa42a888a5d4b95d86f41f31e8cc055b3808
S-Di-S <84771845+S-Di-S@users.noreply.github.com>
2021-07-22 11:52:44 +0300

    Update main.swift

--- Lesson 4/main.swift
+++ Lesson 4/main.swift
@@ -10,3 +10,53 @@ import Foundation
 
 print("Hello, World!")
 
+import AppKit
+
+enum nitro{
+    case nitroOn
+    case nitroOff
+}
+enum luggage{
+    case trunkIsFull
+    case TrunkIsEmpty
+}
+class Car{
+    var brand: String
+    var release: Int
+    init(brand: String, release: Int){
+        self.brand = brand
+        self.release = release
+    }
+}
+class trunkCar: Car{
+    var trunk: luggage
+    init(brand: String, release: Int, trunk: luggage){
+        self.trunk = trunk
+        super.init(brand: brand, release: release)
+    }
+    func trunkState(){
+        if trunk == .trunkIsFull {
+            print("Багажник полон")
+        } else{
+            print("Багажник пуст")
+        }
+    }
+}
+class sportCar: Car{
+    var goFast: nitro
+    init(brand: String, release: Int, goFast: nitro){
+        self.goFast = goFast
+        super.init(brand: brand, release: release)
+    }
+    func nitroState(){
+        if goFast == .nitroOn {
+            print("Спортивная машина едет быстрее")
+        } else{
+            print("Спортивная машина едет медленее")
+        }
+    }
+}
+let gazel = trunkCar(brand: "Газель", release: 2005, trunk: .trunkIsFull)
+gazel.trunkState()
+let audi = sportCar(brand: "Ауди", release: 2008, goFast: .nitroOn)
+audi.nitroState()
